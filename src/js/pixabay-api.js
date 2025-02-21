e5348782412db38b1c5ee3b11f5c5f2ec823a7cd
Olha_Kadeeva <155827553+OlhaKadeeva@users.noreply.github.com>
2025-02-21 19:41:42 +0100

    Initial commit

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+export default function fetchImages(searchText) {
+  const options = {
+    params: {
+      key: '48865125-4f8597dc0399716036b78fee91',
+      q: searchText,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+    },
+  };
+  return axios.get('https://pixabay.com/api/', options);
+}
---
+++ src/main.js
@@ -0,0 +1,41 @@
+import fetchImages from './js/pixabay-api';
+import {
+  hideLoader,
+  renderImages,
+  showLoader,
+  showMessage,
+  showMessageErr,
+} from './js/render-functions';
+
+const form = document.querySelector('form');
+const input = document.querySelector('#search-text');
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(e) {
+  e.preventDefault();
+
+  const searchText = input.value.trim();
+
+  if (!searchText) return;
+
+  input.value = '';
+
+  showLoader();
+
+  fetchImages(searchText)
+    .then(data => handleSearchResults(data.data.hits))
+    .catch(err => {
+      hideLoader();
+      showMessageErr();
+      console.log(err);
+    });
+
+  function handleSearchResults(images) {
+    if (images.length == 0) {
+      showMessage();
+    }
+
+    renderImages(images);
+  }
+}
