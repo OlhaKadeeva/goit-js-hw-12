3e013cdf9957226bec31b06ba0f0e808076d5208
Olha_Kadeeva <o.kadeeva.n@gmail.com>
2025-02-22 10:27:11 +0100

    Revert "remaster to async"
    This reverts commit 5934f1a36e5a1e398021f5b438346c722895e33f.

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default function fetchImages(searchText) {
   const options = {
     params: {
-      key: '48865125-4f8597dc0399716036b78fee9',
+      key: '48865125-4f8597dc0399716036b78fee91',
       q: searchText,
       image_type: 'photo',
       orientation: 'horizontal',
--- src/main.js
+++ src/main.js
@@ -9,95 +9,33 @@ import {
 
 const form = document.querySelector('form');
 const input = document.querySelector('#search-text');
-const btnLoadMore = document.querySelector('.btn-load');
-const loadElem = document.querySelector('.js-loader');
-const gallery = document.querySelector('.gallery');
-
-let query;
-let page;
-let total;
-let per_page = 20;
 
 form.addEventListener('submit', handleSubmit);
 
-async function handleSubmit(e) {
+function handleSubmit(e) {
   e.preventDefault();
 
   const searchText = input.value.trim();
+
   if (!searchText) return;
+
   input.value = '';
+
   showLoader();
 
-  let page = 1;
-  let query = e.target.elements.query.value;
-
-  try {
-    const result = await fetchArticles(query, page, total, per_page);
-
-    fetchImages(searchText);
-
-    const data = handleSearchResults(result, data.data.hits);
-    total = result.totalResults;
-  } catch {
-    hideLoader();
-    showMessageErr();
-    console.log(err);
-  }
+  fetchImages(searchText)
+    .then(data => handleSearchResults(data.data.hits))
+    .catch(err => {
+      hideLoader();
+      showMessageErr();
+      console.log(err);
+    });
 
   function handleSearchResults(images) {
     if (images.length == 0) {
       showMessage();
     }
+
     renderImages(images);
   }
-  checkBtnStatus();
-  hideSpinner();
-}
-btnLoadMore.addEventListener('click', async () => {
-  page += 1;
-  showSpinner();
-  checkBtnStatus();
-  const result = await fetchArticles(query, page, total, per_page);
-  const markup = articlesTemplate(result.images);
-  gallery.insertAdjacentHTML('beforeend', markup);
-  hideSpinner();
-
-  scrollPage();
-});
-
-function showLoadMoreBtn() {
-  btnLoadMore.disabled = false;
-}
-
-function hideLoadMoreBtn() {
-  btnLoadMore.disabled = true;
-}
-
-function checkBtnStatus(page, total, per_page) {
-  const perPage = 20;
-  const maxPage = Math.ceil(total / perPage);
-
-  if (page >= maxPage) {
-    hideLoadMoreBtn();
-    iziToast.info('This is last page');
-  } else {
-    showLoadMoreBtn();
-  }
-}
-
-function showSpinner() {
-  loadElem.classList.remove('hidden');
-}
-
-function hideSpinner() {
-  loadElem.classList.add('hidden');
-}
-
-function scrollPage() {
-  const info = gallery.firstElementChild.getBoundingClientRect();
-  const height = info.height;
-  scrollBy({
-    behavior: 'smooth',
-    top: height * 2,
-  });
 }
